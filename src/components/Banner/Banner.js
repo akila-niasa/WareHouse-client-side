aecae5a72a484e655986f1d95efc9aa924c80f3b
akila-niasa <akilaniasa@gmail.com>
2022-04-29 00:31:59 +0600

    banner added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <div className=' banner w-100 h-100'>
+            <h1 className=' p-5 mx-5 mb-5 text-light'>Choose Your Laptop</h1>
+            <div className='p-5'>
+                <Link to='/blogs'>
+                <button className=' btn'>
+                    Blogs
+                </button>
+                </Link>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
