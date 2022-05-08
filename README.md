fdb97e8e93884374e3ecd98bf9a47fa11fd3541a
akila-niasa <akilaniasa@gmail.com>
2022-05-08 20:38:45 +0600

    readme updated

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
  Live Link: [https://warehouse-management-2d317.web.app](https://warehouse-management-2d317.web.app).
 
+## Short Description 
+In laptor store User first login or resgister.Then he or she updated quantity of product. And he or she see what product he or she added in my items.
 
 ## Features
 * responsive layout.
 * authentication added by using firebase.
 * Private Route added.
+* my items showed those items what user add items.
 * Update Products,Deliver Products, Add Product when User login.
 
 
--- src/components/MySIngleItem/MySIngleItem.js
+++ src/components/MySIngleItem/MySIngleItem.js
@@ -3,13 +3,13 @@ import { Button, Card } from 'react-bootstrap';
 
 import './MySingleItem.css'
 
-const MySIngleItem = ({item,handleDelete}) => {
-    const { name, supplier, quantity,price, image, details, _id } = item
-   
+const MySIngleItem = ({ item, handleDelete }) => {
+    const { name, supplier, quantity, price, image, details, _id } = item
+
     return (
         <div className='row'>
-            
-    <div className='container Card p-3 m-4' >
+
+            <div className='container Card p-3 m-4' >
                 <div className='row'>
                     <div className='col-lg-6'><img src={image} alt="" /></div>
                     <div className='col-lg-6 p-3'>
@@ -18,12 +18,12 @@ const MySIngleItem = ({item,handleDelete}) => {
                         <p>Details: {details}</p>
                         <p>Quantity: {quantity}</p>
                         <p>Price: {price}</p>
-                        <Button onClick={()=>handleDelete(_id)} className='deletButton ms-1' >Delete</Button>
+                        <Button onClick={() => handleDelete(_id)} className='deletButton ms-1' >Delete</Button>
                     </div>
                 </div>
             </div>
 
-  
+
         </div>
     );
 };
